437de5e5a26b6a255f01ce4aebfa1efee295e0df
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-10 10:04:34 +0300

    Project created

---
+++ VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by Sergei Semko on 5/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
